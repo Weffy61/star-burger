9d5786269d5c71a0cabf11bfb89830151fab7a49
Weffy61 <weffy@mail.ru>
2023-12-23 20:56:20 +0300

    Disable interactive user input

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -5,7 +5,7 @@ source venv/bin/activate
 git pull
 pip install -r requirements.txt
 npm ci --dev
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
 python3 manage.py migrate
 systemctl restart star-front.service
 systemctl restart star-back.service
