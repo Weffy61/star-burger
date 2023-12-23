e4520dc587be9f7b201baa78099252d29c377fc6
Weffy61 <weffy@mail.ru>
2023-12-23 20:44:03 +0300

    Update deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -10,4 +10,12 @@ python3 manage.py migrate
 systemctl restart star-front.service
 systemctl restart star-back.service
 systemctl reload nginx.service
+hash=$(git rev-parse HEAD)
+source .env
+curl --http1.1 -X POST \
+  https://api.rollbar.com/api/1/deploy \
+  -H "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{"environment": "'"$ENVIRONMENT"'", "revision": "'"$hash"'"}'
 echo "Деплой успешно произведен"
+
